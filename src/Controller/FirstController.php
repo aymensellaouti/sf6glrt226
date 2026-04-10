b4e219d553f521c56625bfa9926c2c7bc9bb481e
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-10 16:04:04 +0100

    seance du 10 4 2026

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -8,9 +8,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class FirstController extends AbstractController
 {
+
+    #[Route("/", name: 'app_home')]
+    public function home ()
+    {
+        return $this->render('base.html.twig');
+    }
     #[Route('/first', name: 'app_first')]
     public function index(): Response
     {
         return $this->render('first/index.html.twig');
+    }
+
+    #[Route('/notes/{nb?5}', name: 'app_note')]
+    public function notes($nb): Response
+    {
+        $notes = [];
+        for($i=0; $i<$nb;$i++) {
+            $notes[$i] = rand(0,20);
+        }
+        return $this->render('first/notes.html.twig', [
+            'notes' => $notes
+        ]);
     }
 }
--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,9 +8,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 final class HelloController extends AbstractController
 {
-    #[Route('/hello/{name}/{firstname}', name: 'app_hello')]
+    #[Route('/hello/{name}/{firstname}/{age<\d{1,2}>}', name: 'app_hello')]
     public function index($name, $firstname, Request $request): Response
     {
         dump($request);
@@ -26,12 +27,33 @@ final class HelloController extends AbstractController
     #[Route('/session', name: 'app_session')]
     public function session(SessionInterface $session): Response
     {
+        // N7adhrou el data eli bech naffichiwha
+        // Traitement
         if(!$session->has('nbVisite')){
             $this->addFlash('happy','Première visite on est heureux de vous accueillir');
             $session->set('nbVisite', 1);
         } else {
             $session->set('nbVisite', $session->get('nbVisite') + 1);
         }
+
+        // Bech neb3eth ngénéri el page avec les données eli 7adhernahom
         return $this->render('hello/session.html.twig',);
+        //return $this->json('data');
     }
+
+//    /**
+//     * @param SessionInterface $session
+//     * @return Response
+//     * @Route('/session', name:"app_session2")
+//     */
+//    public function session2(SessionInterface $session): Response
+//    {
+//        if(!$session->has('nbVisite')){
+//            $this->addFlash('happy','Première visite on est heureux de vous accueillir');
+//            $session->set('nbVisite', 1);
+//        } else {
+//            $session->set('nbVisite', $session->get('nbVisite') + 1);
+//        }
+//        return $this->render('hello/session.html.twig',);
+//    }
 }
