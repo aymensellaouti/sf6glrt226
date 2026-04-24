7d6165668f7fbd0ab204e774a6b9192d0824f8f3
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-24 14:17:44 +0100

    seance doctrine du 24 4 2026

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Person;
+use App\Repository\PersonRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PersonController extends AbstractController
+{
+    protected ManagerRegistry $doctrine;
+    protected  ObjectManager $manager;
+    protected PersonRepository $personRepository;
+    public function __construct(ManagerRegistry $doctrine) {
+        $this->doctrine = $doctrine;
+        $this->manager = $doctrine->getManager();
+        $this->personRepository = $this->manager->getRepository(Person::class);
+    }
+    #[Route('/person', name: 'app_person')]
+    public function index(): Response
+    {
+        $persons = $this->personRepository->findAll();
+        return $this->render('person/index.html.twig', [
+            'persons' => $persons,
+        ]);
+    }
+
+    #[Route('/person/add', name: 'app_add_person')]
+    public function add(): Response
+    {
+        $person = new Person();
+        $person->setName("salma");
+        $person->setAge(20);
+        $this->manager->persist($person);
+        $person2 = new Person();
+        $person2->setName("Mohamed");
+        $person2->setAge(25);
+        $this->manager->persist($person2);
+        $this->manager->flush();
+        $this->manager->flush();
+        return $this->redirectToRoute('app_person');
+    }
+
+    #[Route('/person/{id}', name: 'app_detail_person')]
+    public function details(Person $person = null): Response
+    {
+        //$person = $this->personRepository->find($id);
+        if (!$person) {
+            throw $this->createNotFoundException('Person not found');
+        }
+        return $this->render('person/index.html.twig', [
+            'persons' => [$person],
+        ]);
+    }
+    #[Route('/person/remove/{id}', name: 'app_remove_person')]
+    public function remove(Person $person = null): Response
+    {
+        //$person = $this->personRepository->find($id);
+        if (!$person) {
+            throw $this->createNotFoundException('Person not found');
+        }
+        $this->manager->remove($person);
+        $this->manager->flush();
+        return $this->redirectToRoute('app_person');
+    }
+
+
+}
